c1a9e18dcaf2ba8b14a9af850511e2fff3a73b65
Albert <albert@diskus.studio>
2019-06-13 14:29:28 +0200

    add async functions

--- rename.js
+++ rename.js
@@ -1,16 +1,20 @@
 let fs = require('fs')
-// let titleCase = require('./titleCase')
 
-function rename (fullPathname, filename, prefix) {
+async function rename (fullPathname, filename, prefix) {
 
-    // get path without filename
-    let pathWithoutFile = fullPathname.replace(filename,'')
-    console.log('path without filename: ',pathWithoutFile)
+    try {
+        // get path without filename
+        let pathWithoutFile = fullPathname.replace(filename,'')
 
-    // rename file and save it
-    fs.rename(`${fullPathname}`,`${pathWithoutFile}${prefix} - ${filename}`, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
-    })
+        // rename file and save it
+        fs.rename(`${fullPathname}`,`${pathWithoutFile}${prefix} - ${filename}`, function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+        })
+    }
+
+    catch(e) {
+        console.error(e)
+    }
 
 }
 

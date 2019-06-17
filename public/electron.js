f671716d4dfbb67fb4af5557c0009aa136af8347
Albert <albert@diskus.studio>
2019-06-17 23:29:56 +0200

    add global shortcut

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, systemPreferences } = require('electron');
+const { app, BrowserWindow, systemPreferences, globalShortcut } = require('electron');
 
 const path = require('path');
 const isDev = require('electron-is-dev');
@@ -32,7 +32,12 @@ function createWindow() {
     mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow()
+    globalShortcut.register('Cmd+,', () => {
+        console.log('Cmd+Comma is pressed')
+    })
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
--- src/components/test.js
+++ src/components/test.js
@@ -8,7 +8,7 @@ export default function test() {
         <div className="app">
             <TitleBar />
             <div class="main-margin justify-center">
-                <h1>Temporary Settings</h1>
+                <h1 className='antialiased text-5xl font-medium'>Temporary Settings</h1>
                 <h1>Watch this space.</h1>
                 <Link to='/'>Go back.</Link>
             </div>
